61c55fcf970ddcf577dcc9abe84061fa1eb0b653
EmperorOfBananas <acerenn01@gmail.com>
2023-03-30 00:06:37 -0400

    testing loading the login http

---
+++ load.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, between, task, SequentialTaskSet
+import json
+
+class MicroUser(HttpUser):
+    wait_time = between(1, 2)
+    host = "http://localhost:5000"
+
+    @task
+    def launch(self):
+        with self.client.post('/auth/login', data=json.dumps({'username': 'Test', 'password': 'TestPass01$'}),
+                        headers={},
+                        name='Test 0',
+                        catch_response=True
+        ) as response:
+            if response.status_code is 200:
+                print(response.status_code)
+                response.success()
+            else:
+                response.failure('failed')
