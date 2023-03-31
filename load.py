434cbaa6cc6352c2be01afa7d3ee5b31781b8e4c
EmperorOfBananas <acerenn01@gmail.com>
2023-03-31 19:17:00 -0400

    testing loginForm

--- load.py
+++ load.py
@@ -1,12 +1,19 @@
 from locust import HttpUser, between, task, SequentialTaskSet
 import json
+from app.forms import LoginForm
 
 class MicroUser(HttpUser):
     wait_time = between(1, 2)
     host = "http://localhost:5000"
 
     @task
-    def launch(self):
+    def login(self):
+        form = LoginForm()
+        form.username.data = 'Test9'
+        form.username.data = 'TestPass01$'
+        response = self.client.post('/auth/login', data=form.data, follow_redirects=True)
+        assert response.status_code == 200
+        '''
         with self.client.post('/auth/login', data=json.dumps({'username': 'Test9', 'password': 'TestPass01$'}),
                         headers={},
                         name='Test 0',
@@ -17,3 +24,4 @@ class MicroUser(HttpUser):
                 response.success()
             else:
                 response.failure('failed')
+                '''
